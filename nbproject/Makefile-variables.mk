a6eea69a1301f9913f29cc6f4e078084a499f8dd
robertferanec <5564443+robertferanec@users.noreply.github.com>
2021-05-19 20:54:39 +0200

    Initial commit - simple RGB PWM Working

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Tiny.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Tiny.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=tiny.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/tiny.x.tar
